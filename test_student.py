4aa46af708e2246a0a0e48c5c11adb45145363b2
Abhishek Mahida <abhishekmahida56@gmail.com>
2025-02-13 18:55:54 +0000

    unittesting

--- test_student.py
+++ test_student.py
@@ -1,23 +1,67 @@
-
 import unittest
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        print("tearDown")
+
     def test_full_name(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        student = Student("John", "Doe")
-        student.alert_santa()
-        self.assertTrue(student.naughty_list)
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
-    def test_email_return(self):
+    def test_email(self):
+        print("test_email")
 
-        student = Student("John", "Doe")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
-        self.assertEqual(student.email, 'john.doe@email.com')
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(
+            self.student.end_date,
+            old_end_date + timedelta(days=5))
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failed(self):
+        """
+        In the path "student" comes from the name of the file student.py
+        If you have named the file something else - it will need to match
+        eg, students.py would need "students.requests.get"
+        """
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong")
 
 
 if __name__ == "__main__":
